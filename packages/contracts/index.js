219676a6456473cc2fbce2d1ac29fdbe0902c963
Blaine Bublitz <blaine.bublitz@gmail.com>
2022-09-21 11:40:17 -0700

    reset contracts

--- packages/contracts/index.js
+++ packages/contracts/index.js
@@ -1,3 +1,4 @@
+"use strict";
 /**
  * This package contains deployed contract addresses, ABIs, and Typechain types
  * for the Dark Forest game.
@@ -30,24 +31,26 @@
  *
  * @packageDocumentation
  */
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.INIT_ADDRESS = exports.CONTRACT_ADDRESS = exports.START_BLOCK = exports.NETWORK_ID = exports.NETWORK = void 0;
 /**
  * The name of the network where these contracts are deployed.
  */
-export const NETWORK = 'localhost';
+exports.NETWORK = 'localhost';
 /**
  * The id of the network where these contracts are deployed.
  */
-export const NETWORK_ID = 31337;
+exports.NETWORK_ID = 31337;
 /**
  * The block in which the DarkForest contract was initialized.
  */
-export const START_BLOCK = 0;
+exports.START_BLOCK = 0;
 /**
  * The address for the DarkForest contract.
  */
-export const CONTRACT_ADDRESS = '0x8950bab77f29E8f81e6F78AEA0a79bADD88Eeb13';
+exports.CONTRACT_ADDRESS = '0x8950bab77f29E8f81e6F78AEA0a79bADD88Eeb13';
 /**
  * The address for the initalizer contract. Useful for lobbies.
  */
-export const INIT_ADDRESS = '0x500cf53555c09948f4345594F9523E7B444cD67E';
+exports.INIT_ADDRESS = '0x500cf53555c09948f4345594F9523E7B444cD67E';
 //# sourceMappingURL=index.js.map
