0608220432f05b1c42528f18bc3fe11aac113713
Blaine Bublitz <blaine.bublitz@gmail.com>
2022-09-22 15:01:24 -0700

    feat: Convert sample component to Svelte & write some tests

---
+++ client/src/Frontend/Components/Icons/Icons.test.ts
@@ -0,0 +1,36 @@
+import { cleanup, render, screen } from '@testing-library/svelte';
+import { afterEach, expect } from 'vitest';
+import Quasar from './Quasar.svelte';
+
+afterEach(() => cleanup());
+
+test('Quasar icon: default', () => {
+  render(Quasar);
+
+  const el = screen.getByRole('img');
+  expect(el).toBeTruthy();
+  expect(el.getAttribute('alt')).toEqual('Quasar');
+  expect(el.getAttribute('src')?.endsWith('.svg')).toEqual(true);
+  expect(el.getAttribute('width')).toEqual('48px');
+  expect(el.getAttribute('height')).toEqual('48px');
+});
+
+test('Quasar icon: custom height & width', () => {
+  render(Quasar, { height: '24px', width: '24px' });
+
+  const el = screen.getByRole('img');
+  expect(el).toBeTruthy();
+  expect(el.getAttribute('width')).toEqual('24px');
+  expect(el.getAttribute('height')).toEqual('24px');
+});
+
+test('Quasar icon: rerender with new prop', () => {
+  const { rerender } = render(Quasar);
+
+  rerender({ width: '96px' });
+
+  const el = screen.getByRole('img');
+  expect(el).toBeTruthy();
+  expect(el.getAttribute('width')).toEqual('96px');
+  expect(el.getAttribute('height')).toEqual('48px');
+});
