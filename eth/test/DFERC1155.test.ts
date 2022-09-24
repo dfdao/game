832b27db667a4e8a7cc2f0c57c2825cca89d3a58
cha0sg0d <tony.goss@protonmail.com>
2022-09-24 14:43:46 +0100

    test: working mint and uri tests

--- eth/test/DFERC1155.test.ts
+++ eth/test/DFERC1155.test.ts
@@ -1,17 +1,47 @@
 import { DFToken, DFToken__factory } from '@dfdao/contracts/typechain';
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
+import { expect } from 'chai';
 import { ethers } from 'hardhat';
 
-const tokenURI = 'ERC1155Metadata.tokenURI';
+/**
+ * Once again, a friendly reminder that in ERC1155 tokenId refers to a collection of 1 or more
+ * tokens, NOT guaranteed to be unique instances of tokens.
+ */
 
 describe('SolidStateERC1155', function () {
   let token: DFToken;
+  let deployer: SignerWithAddress;
+  let user1: SignerWithAddress;
+  const addressZero = ethers.constants.AddressZero;
+  const collectionId = ethers.constants.Zero;
+  const amount = ethers.constants.Two;
+  const tokenURI = 'ERC1155Metadata.tokenURI/{id}.json';
 
   beforeEach(async function () {
-    const [deployer] = await ethers.getSigners();
+    const signers = await ethers.getSigners();
+    deployer = signers[0];
     token = await new DFToken__factory(deployer).deploy();
+    user1 = signers[1];
   });
 
-  it('mints', async function () {
-    console.log('not yet!');
+  it('mints tokens for tokenId zero', async function () {
+    await expect(token.mint(user1.address, collectionId, amount, ethers.constants.HashZero))
+      .to.emit(token, 'TransferSingle')
+      .withArgs(deployer.address, addressZero, user1.address, collectionId, amount);
+
+    await expect(
+      token.connect(user1).mint(user1.address, collectionId, amount, ethers.constants.HashZero)
+    )
+      .to.emit(token, 'TransferSingle')
+      .withArgs(user1.address, addressZero, user1.address, collectionId, amount);
+    // Each mint created 2 tokens in the same collection. Two mints = 4 tokens.
+    expect(await token.balanceOf(user1.address, collectionId)).to.equal(amount.mul(2));
+  });
+  it('sets tokenURI for tokenId zero', async function () {
+    await expect(token.setTokenURI(collectionId, tokenURI))
+      .to.emit(token, 'URI')
+      .withArgs(tokenURI, collectionId);
+
+    expect(await token.uri(collectionId)).to.equal(tokenURI);
   });
 });
