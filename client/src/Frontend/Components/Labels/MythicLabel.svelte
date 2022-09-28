d26feef66d8237bf2a89c8a3d344d0b34a1f8b34
Blaine Bublitz <blaine.bublitz@gmail.com>
2022-09-28 10:35:42 -0700

    feat: Migrate MythicLabel component

---
+++ client/src/Frontend/Components/Labels/MythicLabel.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  export let text: string;
+</script>
+
+<span class="Anim">
+  {#each text as c, i}
+    <span class="AnimDelay" style:--index={-i}>
+      {c}
+    </span>
+  {/each}
+</span>
+
+<style>
+  @keyframes color {
+    0% {
+      filter: hue-rotate(0);
+    }
+    30% {
+      filter: hue-rotate(0);
+    }
+    100% {
+      filter: hue-rotate(360deg);
+    }
+  }
+
+  .Anim {
+    color: var(--df-colors-artifact-rarity-mythic);
+  }
+
+  .AnimDelay {
+    animation: color 1s ease-in-out infinite alternate;
+    animation-delay: calc(var(--index) * 0.04s);
+  }
+</style>
